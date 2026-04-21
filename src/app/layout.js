37288cd082179b5c5bca6d9f7df371614ca5994a
Mitchell <mitchelljamesb99@gmail.com>
2026-04-20 17:22:06 -0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,6 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from './compenents/Navbar'
 import Footer from './compenents/Footer'
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +23,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Navbar />
-        <Analytics />
         {children}
         <Footer />
       </body>
