c9ba3ee6b362f68e022c15e012c19ce0e2e97c52
Mitchell <mitchelljamesb99@gmail.com>
2026-04-22 18:57:36 -0700

    testing out fix for deployment error again

--- src/app/compenents/Tag.js
+++ src/app/compenents/Tag.js
@@ -1,4 +1,9 @@
-export default function(content = 'New', classNames = '') {
-  return <div className={'px-2 b-gradient-to-r from-cyan-500 to-red-500 text-white rounded-full text-mono ${classNames}'}>
-    {content}</div>
+export default function({content = 'New', classNames = ''}) {
+  return (
+   <div
+      className={'px-2 b-gradient-to-r from-cyan-500 to-red-500 text-white rounded-full text-mono ${classNames}'}
+  >
+    {content}
+  </div>
+  )
 }
