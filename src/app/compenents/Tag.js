e19442d868580452c9ff1946ffc9bca77a5568c6
Mitchell <mitchelljamesb99@gmail.com>
2026-04-22 19:00:47 -0700

    fixed button issue

--- src/app/compenents/Tag.js
+++ src/app/compenents/Tag.js
@@ -1,7 +1,7 @@
 export default function({content = 'New', classNames = ''}) {
   return (
    <div
-      className={'px-2 b-gradient-to-r from-cyan-500 to-red-500 text-white rounded-full text-mono ${classNames}'}
+      className={'px-2 bg-gradient-to-r from-cyan-500 to-red-500 text-white rounded-full text-mono ${classNames}'}
   >
     {content}
   </div>
